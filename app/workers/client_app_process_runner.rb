6aaabc5b9fc6ec1cbe620a942c4e6ca8c9633db6
Bookis Smuin <vegan.bookis@gmail.com>
2012-11-14 00:14:23 -0700

    change process runner to only migrate

--- app/workers/client_app_process_runner.rb
+++ app/workers/client_app_process_runner.rb
@@ -1,8 +1,8 @@
 class ClientAppProcessRunner
   @queue = :process_runner
 
-  def self.perform(client_app_id, command)
+  def self.perform(client_app_id)
     client_app = ClientApp.find(client_app_id)
-    client_app.run(command)
+    client_app.run('rake db:migrate')
   end
 end
