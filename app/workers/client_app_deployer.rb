227f9072224da4ef46428ab55c0ecb31b1b1a736
Bookis Smuin <vegan.bookis@gmail.com>
2012-11-14 00:16:55 -0700

    remove command from resque args

--- app/workers/client_app_deployer.rb
+++ app/workers/client_app_deployer.rb
@@ -4,7 +4,7 @@ class ClientAppDeployer
   def self.perform(client_app_id)
     client_app = ClientApp.find(client_app_id)
     if client_app.deploy
-      Resque.enqueue(ClientAppProcessRunner, client_app_id, "rake db:migrate")
+      Resque.enqueue(ClientAppProcessRunner, client_app_id)
     end
   end
   
