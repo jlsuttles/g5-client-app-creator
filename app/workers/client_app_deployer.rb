b99df6635a720388eb876245a9ee8c336518fd6d
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-20 13:56:59 -0800

    fixes typo

--- app/workers/client_app_deployer.rb
+++ app/workers/client_app_deployer.rb
@@ -6,7 +6,7 @@ class ClientAppDeployer
     if client_app.deploy
       client_app.heroku_run("rake db:migrate")
       client_app.heroku_config_set("G5_CLIENT_UID" => client_app.uid)
-      client_app.heroku_run("rake seed_client") if client.app.name.include? "g5-ch-"
+      client_app.heroku_run("rake seed_client") if client_app.name.include? "g5-ch-"
     end
   end
 end
