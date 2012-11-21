65e7facadaa6abd7899b2e3ebf88f8d341779ca4
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-21 14:58:16 -0800

    sleep 30

--- app/workers/client_app_deployer.rb
+++ app/workers/client_app_deployer.rb
@@ -6,7 +6,7 @@ class ClientAppDeployer
     if client_app.deploy
       client_app.heroku_run("rake db:migrate")
       client_app.heroku_config_set("G5_CLIENT_UID" => client_app.client_uid)
-      client_app.heroku_restart
+      sleep 30
       client_app.heroku_run("rake seed_client") if client_app.name.include? "g5-ch-"
     end
   end
