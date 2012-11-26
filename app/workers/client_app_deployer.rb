0953135bcaf36b341a17f645c6d67af498585a43
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-26 14:14:29 -0800

    autoscales client app deployer worker when in production

--- app/workers/client_app_deployer.rb
+++ app/workers/client_app_deployer.rb
@@ -1,4 +1,5 @@
 class ClientAppDeployer
+  extend HerokuResqueAutoscaler if Rails.env.production?
   @queue = :deployer
 
   def self.perform(client_app_id)
@@ -6,7 +7,7 @@ class ClientAppDeployer
     if client_app.deploy
       client_app.heroku_run("rake db:migrate")
       client_app.heroku_config_set("G5_CLIENT_UID" => client_app.client_uid)
-      sleep 30
+      sleep 10
       client_app.heroku_run("rake seed_client") if client_app.name.include? "g5-ch-"
     end
   end
