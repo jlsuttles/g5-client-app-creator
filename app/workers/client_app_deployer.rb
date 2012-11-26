a11f2013c5897623c482782b2506115c7938e290
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-26 15:24:44 -0800

    sets ENV vars for g5-ch-* and g5-chd-*

--- app/workers/client_app_deployer.rb
+++ app/workers/client_app_deployer.rb
@@ -4,11 +4,31 @@ class ClientAppDeployer
 
   def self.perform(client_app_id)
     client_app = ClientApp.find(client_app_id)
+
+    puts "Deploying #{client_app.name}..."
     if client_app.deploy
+
+      puts "Migrating..."
       client_app.heroku_run("rake db:migrate")
-      client_app.heroku_config_set("G5_CLIENT_UID" => client_app.client_uid)
-      sleep 10
-      client_app.heroku_run("rake seed_client") if client_app.name.include? "g5-ch-"
+
+      puts "Setting config variables..."
+      client_app.heroku_config_set(
+        "G5_CLIENT_UID" => client_app.client_uid,
+        "HEROKU_API_KEY" => ENV["HEROKU_API_KEY"],
+        "HEROKU_APP_NAME" => client_app.name,
+        "HEROKU_USERNAME" => ENV["HEROKU_USERNAME"],
+        "ID_RSA" => ENV["ID_RSA"],
+      )
+
+      # Client Hubs
+      if client_app.name.include? "g5-ch-"
+        puts "Taking a nap..."
+        sleep 10
+        puts "Seeding database..."
+        client_app.heroku_run("rake seed_client")
+      end
+
+      puts "Done deploying #{client_app.name}"
     end
   end
 end
