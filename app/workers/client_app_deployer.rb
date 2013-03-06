6cd0a8171ab87a83f7546c29d8c4ce0ea82e701e
Jessica Lynn Suttles & Michael Mitchell <g5dev+jls+mj@g5searchmarketing.com>
2013-03-06 11:38:44 -0800

    add setup for client lead service app

--- app/workers/client_app_deployer.rb
+++ app/workers/client_app_deployer.rb
@@ -11,37 +11,47 @@ class ClientAppDeployer
       puts "Migrating..."
       client_app.heroku_run("rake db:migrate")
 
-      puts "Setting config variables..."
-      client_app.heroku_config_set(
-        # for targeting
-        "UID" => client_app.uid,
-        # for sibling deployer
-        "MAIN_APP_UID" => client_app.uid,
-        # for seeding client info
-        "G5_CLIENT_UID" => client_app.client_uid,
-        # for autoscaling
-        "HEROKU_APP_NAME" => client_app.name,
-        # for deploying
-        "HEROKU_API_KEY" => ENV["HEROKU_API_KEY"],
-        "HEROKU_USERNAME" => ENV["HEROKU_USERNAME"],
-        "ID_RSA" => ENV["ID_RSA"],
-      )
-
       sleep 15 unless Rails.env.test?
       puts "Adding resdistogo:nano"
       client_app.heroku_addon_add("redistogo:nano")
       sleep 15 unless Rails.env.test?
 
-      if client_app.name.include? "g5-ch-"
-        puts "Seeding database..."
-        client_app.heroku_run("rake seed_client")
-        client_app.heroku_run("rake sibling:consume")
-      elsif client_app.name.include? "g5-chd-"
-        puts "Seeding database..."
-        client_app.heroku_run("rake sibling:consume")
-      end
+      setup_client_hub(client_app) if client_app_name.include? "g5-ch"
+      setup_client_service(client_app) if client_app_name.include? "g5-cls-"
 
       puts "Done deploying #{client_app.name}"
+    end
+  end
+
+  def self.setup_client_service(client_app)
+    puts "Scaling worker to 1..."
+    client_app.heroku_post_ps_scale("worker", 1)
+  end
+
+  def self.setup_client_hub(client_app)
+    puts "Setting config variables..."
+    client_app.heroku_config_set(
+      # for targeting
+      "UID" => client_app.uid,
+      # for sibling deployer
+      "MAIN_APP_UID" => client_app.uid,
+      # for seeding client info
+      "G5_CLIENT_UID" => client_app.client_uid,
+      # for autoscaling
+      "HEROKU_APP_NAME" => client_app.name,
+      # for deploying
+      "HEROKU_API_KEY" => ENV["HEROKU_API_KEY"],
+      "HEROKU_USERNAME" => ENV["HEROKU_USERNAME"],
+      "ID_RSA" => ENV["ID_RSA"],
+    )
+
+    if client_app.name.include? "g5-ch-"
+      puts "Seeding database..."
+      client_app.heroku_run("rake seed_client")
+      client_app.heroku_run("rake sibling:consume")
+    elsif client_app.name.include? "g5-chd-"
+      puts "Seeding database..."
+      client_app.heroku_run("rake sibling:consume")
     end
   end
 end
