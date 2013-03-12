0a0b84b719d825e2c269a5b44ad779b85f457779
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-03-11 19:32:46 -0700

    adds default values for env variables in config/initializers/env.rb

---
+++ config/initializers/env.rb
@@ -0,0 +1,11 @@
+ENV["G5_CONFIGURATOR_FEED_URL"] ||= case Rails.env
+  when "production" then "http://g5-configurator.herokuapp.com"
+  when "development" then "http://g5-configurator.dev"
+  when "test" then "http://g5-configurator.test"
+end
+
+ENV["G5_CLIENT_APP_CREATOR_UID"] ||= case Rails.env
+  when "production" then "http://g5-configurator.herokuapp.com/apps/g5-client-app-creator"
+  when "development" then "http://g5-configurator.dev/apps/g5-client-app-creator"
+  when "test" then "http://g5-configurator.test/apps/g5-client-app-creator"
+end
