cff77b58ef0e210368804f4157e4be2381a0cab2
Chad Crissman <cdcrissman@gmail.com>
2014-03-12 15:55:52 -0400

    update config files

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'rails/all'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+Bundler.require(:default, Rails.env)
+
 
 module G5ClientAppCreator
   class Application < Rails::Application
